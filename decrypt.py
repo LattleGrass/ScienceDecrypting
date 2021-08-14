f20476fa2ba99f945a5dd64885eb77caea4399ea
Eumeryx <eumeryx@foxmali.com>
2021-08-14 17:22:49 +0800

    fix Unable to find 'endstream' marker after stream

--- decrypt.py
+++ decrypt.py
@@ -39,6 +39,153 @@ def aes_decrypt(key, iv, data, pad=False):
     return unpadder.update(ret) + unpadder.finalize()
 
 
+class MyDictionaryObject(DictionaryObject):
+    def readFromStream(stream, pdf):
+        debug = False
+        tmp = stream.read(2)
+        if tmp != b_("<<"):
+            raise utils.PdfReadError("Dictionary read error at byte %s: stream must begin with '<<'" % utils.hexStr(stream.tell()))
+        data = {}
+        while True:
+            tok = readNonWhitespace(stream)
+            if tok == b_('\x00'):
+                continue
+            elif tok == b_('%'):
+                stream.seek(-1, 1)
+                skipOverComment(stream)
+                continue
+            if not tok:
+                # stream has truncated prematurely
+                raise PdfStreamError("Stream has ended unexpectedly")
+
+            if debug: print(("Tok:", tok))
+            if tok == b_(">"):
+                stream.read(1)
+                break
+            stream.seek(-1, 1)
+            key = readObject(stream, pdf)
+            tok = readNonWhitespace(stream)
+            stream.seek(-1, 1)
+            value = readObject(stream, pdf)
+            if not data.get(key):
+                data[key] = value
+            elif pdf.strict:
+                # multiple definitions of key not permitted
+                raise utils.PdfReadError("Multiple definitions in dictionary at byte %s for key %s" \
+                                           % (utils.hexStr(stream.tell()), key))
+            else:
+                warnings.warn("Multiple definitions in dictionary at byte %s for key %s" \
+                                           % (utils.hexStr(stream.tell()), key), utils.PdfReadWarning)
+
+        pos = stream.tell()
+        s = readNonWhitespace(stream)
+        if s == b_('s') and stream.read(5) == b_('tream'):
+            eol = stream.read(1)
+            # odd PDF file output has spaces after 'stream' keyword but before EOL.
+            # patch provided by Danial Sandler
+            while eol == b_(' '):
+                eol = stream.read(1)
+            assert eol in (b_("\n"), b_("\r"))
+            if eol == b_("\r"):
+                # read \n after
+                if stream.read(1)  != b_('\n'):
+                    stream.seek(-1, 1)
+            # this is a stream object, not a dictionary
+            assert "/Length" in data
+            length = data["/Length"]
+            if debug: print(data)
+            stream_start = stream.tell()
+            if isinstance(length, IndirectObject):
+                length = pdf.getObject(length)
+                stream.seek(stream_start, 0)
+            data["__streamdata__"] = stream.read(length)
+            if debug: print("here")
+            #if debug: print(binascii.hexlify(data["__streamdata__"]))
+            e = readNonWhitespace(stream)
+            ndstream = stream.read(8)
+            if (e + ndstream) != b_("endstream"):
+                # (sigh) - the odd PDF file has a length that is too long, so
+                # we need to read backwards to find the "endstream" ending.
+                # ReportLab (unknown version) generates files with this bug,
+                # and Python users into PDF files tend to be our audience.
+                # we need to do this to correct the streamdata and chop off
+                # an extra character.
+                pos = stream.tell()
+                stream.seek(-10, 1)
+                end = stream.read(9)
+                if end == b_("endstream") or b_("EOF") in end:
+                    # we found it by looking back one character further.
+                    data["__streamdata__"] = data["__streamdata__"][:-1]
+                else:
+                    # Handle stream that is few bytes longer than expected
+                    stream.seek(stream_start + length, 0)
+                    extra = stream.read(100)
+                    p = extra.find(b_("endstream"))
+                    if p >= 0:
+                        stream.seek(stream_start + length + p + 9, 0)
+                        extra = extra[:p].rstrip(b_('\r\n '))
+                        data["__streamdata__"] = data["__streamdata__"] + extra
+                    else:
+                        if debug: print(("E", e, ndstream, debugging.toHex(end)))
+                        stream.seek(pos, 0)
+                        raise utils.PdfReadError("Unable to find 'endstream' marker after stream at byte %s." % utils.hexStr(stream.tell()))
+        else:
+            stream.seek(pos, 0)
+        if "__streamdata__" in data:
+            return StreamObject.initializeFromDictionary(data)
+        else:
+            retval = DictionaryObject()
+            retval.update(data)
+            return retval
+
+
+def readObject(stream, pdf):
+    tok = stream.read(1)
+    stream.seek(-1, 1) # reset to start
+    idx = ObjectPrefix.find(tok)
+    if idx == 0:
+        # name object
+        return NameObject.readFromStream(stream, pdf)
+    elif idx == 1:
+        # hexadecimal string OR dictionary
+        peek = stream.read(2)
+        stream.seek(-2, 1) # reset to start
+        if peek == b_('<<'):
+            return MyDictionaryObject.readFromStream(stream, pdf)
+        else:
+            return readHexStringFromStream(stream)
+    elif idx == 2:
+        # array object
+        return ArrayObject.readFromStream(stream, pdf)
+    elif idx == 3 or idx == 4:
+        # boolean object
+        return BooleanObject.readFromStream(stream)
+    elif idx == 5:
+        # string object
+        return readStringFromStream(stream)
+    elif idx == 6:
+        # null object
+        return NullObject.readFromStream(stream)
+    elif idx == 7:
+        # comment
+        while tok not in (b_('\r'), b_('\n')):
+            tok = stream.read(1)
+        tok = readNonWhitespace(stream)
+        stream.seek(-1, 1)
+        return readObject(stream, pdf)
+    else:
+        # number object OR indirect reference
+        if tok in NumberSigns:
+            # number
+            return NumberObject.readFromStream(stream)
+        peek = stream.read(20)
+        stream.seek(-len(peek), 1) # reset to start
+        if IndirectPattern.match(peek) != None:
+            return IndirectObject.readFromStream(stream, pdf)
+        else:
+            return NumberObject.readFromStream(stream)
+
+
 class MyPdfFileReader(PyPDF2.PdfFileReader):
     def SetFileKey(self, key):
         self._decryption_key = key
